8c4d5a09c276697b0b62a445cf44295819ea670f
Julian Lannigan <julian.lannigan@gmail.com>
2022-04-13 00:44:08 -0400

    chore: embedded structs

--- structs/main.go
+++ structs/main.go
@@ -5,13 +5,23 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
-	var alex person
+	jimmy := person{
+		firstName: "Jimmy",
+		lastName:  "Toledo",
+		contact: contactInfo{
+			email:   "jimmy@trucks.com",
+			zipCode: 43056,
+		},
+	}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-
-	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", jimmy)
 }
